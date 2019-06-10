509265c273683beb35d69566b23e59cda3032d99
Maria M <marsed@mail.ru>
2019-06-10 22:37:16 +0400

    update .gitignore

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+// Вывод ошибок на экран (для удобной отладки)
+// $configuration = [
+//     'settings' => [
+//             'displayErrorDetails' => true,
+//                 ],
+//                 ];
+//
+//                 $app = new \Slim\App($configuration);
+//
+//                 $app->get('/', function ($request, $response) {
+//                     return $response->write('Welcome to Slim!');
+//                     });
+//                     $app->run();
+//
